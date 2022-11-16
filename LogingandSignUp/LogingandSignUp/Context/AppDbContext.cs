f3fa5e338a56f409336c360216ba9b336a14cf35
zee-creato <85754092+zee-creato@users.noreply.github.com>
2022-11-16 20:03:26 +0000

    first commit

---
+++ LogingandSignUp/LogingandSignUp/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using LogingandSignUp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogingandSignUp.Context
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<User>Users { get; set; }   
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");  
+        }
+
+
+
+    }
+}
---
+++ LogingandSignUp/LogingandSignUp/Controllers/UserController.cs
@@ -0,0 +1,152 @@
+﻿using LogingandSignUp.Context;
+using LogingandSignUp.Helpers;
+using LogingandSignUp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LogingandSignUp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.UserName == userObj.UserName);
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            if(!PasswordHasher.VerifyPassword(userObj.Password,user.Password))
+            {
+                return BadRequest(new { Message = "Password is Incorrect" });
+            }
+            user.Token = CreateJwt(user);
+            return Ok(new
+            {
+                Token= user.Token,
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+            // Check username
+
+            if(await CheckUserNameExistAsync(userObj.UserName))
+            
+                return BadRequest(new {Message = "Username Already Exist!"});
+            
+
+
+            // Check Email
+
+            if (await CheckEmailExistAsync(userObj.Email))
+            
+                return BadRequest(new { Message = "Email Already Exist!" });
+
+
+
+
+            // Check password Strength
+
+            var pass = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass.ToString() });
+
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registred!"
+            });
+
+
+
+
+
+
+
+
+        }
+
+
+
+        private async Task<bool> CheckUserNameExistAsync(string username)
+
+             => await _authContext.Users.AnyAsync(x => x.UserName == username);
+        private async Task<bool> CheckEmailExistAsync(string email)
+
+              => await _authContext.Users.AnyAsync(x => x.Email == email);
+
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(password.Length < 8)
+            sb.Append("Minimum password length should be 8" + Environment.NewLine);
+
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]"))) 
+                sb.Append("Password should be Alphanumeric" + Environment.NewLine);
+            if (!Regex.IsMatch(password, "[<,>,@,!,#,$,%,&,*,^,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))
+            sb.Append("Password should contain special chars" + Environment.NewLine);
+            return sb.ToString();
+           
+        }
+
+
+        private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name,$"{user.FirstName} {user.LastName}")
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                  Expires = DateTime.Now.AddDays(1),
+                //Expires = DateTime.Now.AddSeconds(10),
+                SigningCredentials = credentials
+               
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            return Ok(await _authContext.Users.ToListAsync());
+        }
+    }
+}
